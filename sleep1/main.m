1f6fd6d66b08303feb6852594fae9ef990ee6b1b
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2013-12-29 18:21:52 +1100

    Initial Commit

---
+++ sleep1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sleep1
+//
+//  Created by Park Jung Gyu on 29/12/13.
+//  Copyright (c) 2013 pyfamily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "test1AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([test1AppDelegate class]));
+    }
+}
---
+++ sleep1/test1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  test1AppDelegate.h
+//  sleep1
+//
+//  Created by Park Jung Gyu on 29/12/13.
+//  Copyright (c) 2013 pyfamily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
