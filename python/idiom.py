7d60aad8770cccab61e8014d3d6a143c6c04bdef
U-RESMED\junggyup <junggyup@WRES24057.corp.resmed.org>
2014-06-25 12:31:53 +1000

    added map-reduce example

--- python/idiom.py
+++ python/idiom.py
@@ -610,3 +610,18 @@ a.foo =  lambda x: x + 2
 # you can change the definition of foo in the class level:
 a.__class__.foo = lambda self, x: x  + 10
 
+
+# simple map-reduce in python
+# http://mikecvet.wordpress.com/2010/07/02/parallel-mapreduce-in-python/
+
+# simple map-reduce test
+a = [1, 2, 3]
+b = [4, 5, 6, 7]
+c = [8, 9, 1, 2, 3]
+L = map(lambda x:len(x), [a, b, c])
+ 
+# L == [3, 4, 5]
+N = reduce(lambda x, y: x+y, L)
+# N == 12
+# Or, if we want to be fancy and do it in one line
+N = reduce(lambda x, y: x+y, map(lambda x:len(x), [a, b, c]))
