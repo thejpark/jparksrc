00e9af96ae690eacf24dd20225c7e5f8c316e277
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-06-27 23:10:04 +1000

    added map-test.py

---
+++ python/map-test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from multiprocessing import Pool
+import subprocess
+import os
+
+def foo(x):
+    r = subprocess.Popen(['ls', '-la', '../{}'.format(x)])
+
+if __name__ == '__main__':
+    p = Pool(2)
+    print(p.map(foo, ['java', 'cpp']))
