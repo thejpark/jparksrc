3f536fcce1cbaa18ba254b32dab2bbb2e6e62d5e
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-09-02 07:26:37 +1000

    add android project: geoquiz

---
+++ android/projects/GeoQuiz/src/com/example/geoquiz/QuizActivity.java
@@ -0,0 +1,137 @@
+package com.example.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
+
+public class QuizActivity extends Activity {
+
+    Button mTrueButton;
+    Button mFalseButton;
+    Button mNextButton;
+    TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
+    TrueFalse[] mAnswerKey = new TrueFalse[] {
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, true)
+    };
+    private int mCurrentIndex;
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStart() {
+	super.onStart();
+	Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+	super.onPause();
+	Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+	super.onStop();
+	Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onResume() {
+	super.onResume();
+	Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onDestroy() {
+	super.onDestroy();
+	Log.d(TAG, "onDestroy() called");
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	Log.d(TAG, "onCreate(Bundle) called");
+        setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+	int question = mAnswerKey[mCurrentIndex].getQuestion();
+	mQuestionTextView.setText(question);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+		//  we should not use 'this', as this means the object of the anonymous class
+		checkAnswer(true);
+            }
+        });
+
+        mFalseButton = (Button)findViewById(R.id.false_button);		
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+		checkAnswer(false);
+            }
+        });
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mAnswerKey.length; 
+                updateQuestion();
+            }
+        });
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
+	updateQuestion();
+    }
+
+    private void updateQuestion() {
+        int question = mAnswerKey[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mAnswerKey[mCurrentIndex].isTrueQuestion();
+        
+        int messageResId = 0;
+        
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_quiz, menu);
+        return true;
+    }
+
+}
