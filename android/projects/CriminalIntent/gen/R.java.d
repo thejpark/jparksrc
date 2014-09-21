aa4180f3d7165f031b9e3b5f39814335d3266d65
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-09-21 16:35:56 +1000

    Added criminalintent android project

---
+++ android/projects/CriminalIntent/gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/gen/com/example/criminalintent/R.java \
+ : /Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/res/drawable-hdpi/ic_launcher.png \
+/Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/res/drawable-ldpi/ic_launcher.png \
+/Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/res/drawable-mdpi/ic_launcher.png \
+/Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/res/drawable-xhdpi/ic_launcher.png \
+/Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/res/layout/activity_crime.xml \
+/Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/res/layout/fragment_crime.xml \
+/Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/res/values/strings.xml \
+/Users/jpark/wrk/jparksrc/android/projects/CriminalIntent/bin/AndroidManifest.xml \
