68da2a9cb084e9955a3f80420bd61aa807731f83
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-10-09 06:43:23 +1100

    added integer set class

---
+++ cpp/integer-set.cpp
@@ -0,0 +1,99 @@
+// integer set with adding and deleting in a constant time
+// can we use map only? ( that means, add will take more than O(1).
+
+#include <map>
+
+class mySet {
+
+public:
+    mySet() : head(NULL), tail(NULL)
+    {
+    }
+    bool add(int x);
+    bool remove(int x);
+    bool contains(int  x);
+    int size();
+
+private:
+    class node {
+    public:
+        node(int x) : data(x), prev(NULL), next(NULL) {}
+        int data;
+        node* prev;
+        node* next;
+    };
+    node* head;
+    node* tail;
+    map<int, node*> mc;
+};
+
+bool mySet::add(int x)
+{
+    node n = new node(x);
+    if (!n)
+        return false;
+
+    if (head == NULL)
+        head = tail = n; 
+    else 
+    {
+        tail->next = n;
+        n->prev = tail;
+        tail = n;
+        n->next = NULL;
+    }
+
+    mc[x] = n;
+    count++;
+    return true;
+}
+
+bool mySet::remove(int x)
+{
+    node* n = mc[x];
+    if (!n)
+        return false;
+
+    mc[x] = NULL;
+    count--;
+
+    if (n == head && n == tail)
+    {
+        head = tail = NULL;
+        delete n;
+    }
+    else if (n == head)
+    {
+        head = head->next;
+        head->prev = NULL;
+        delete n;
+    }
+    else if (n == tail)
+    {
+        tail = tail->prev;
+        tail->next = NULL;
+        delete n;
+    }
+    else
+    {
+        n->prev->next = n->next;
+        n->next->prev = n->prev;
+        delete n;
+    }
+
+    return true;
+}
+
+bool mySet::contains(int x)
+{
+    if (mc[x])
+        return true;
+
+    return false;
+}
+
+
+int mySet::size()
+{
+    return count;
+}
