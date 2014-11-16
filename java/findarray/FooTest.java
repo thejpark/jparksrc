37f4681954e03a70c9d5912fa1b5dc2a6b4d0a1e
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-11-16 23:31:57 +1100

    Unittest added

---
+++ java/findarray/FooTest.java
@@ -0,0 +1,35 @@
+package findarray;
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import java.util.*;
+
+/**
+ * Tests for {@link Foo}.
+ *
+ * @author user@example.com (John Doe)
+ */
+public class FooTest {
+
+    @Test
+    public void test1() {
+        int a[] = {4, 9, 3, 7, 8
+        };
+        int b[] = {3, 7
+        };
+
+        MyFindArray fa = new MyFindArray();
+	int i = fa.findArray(a, b);
+	org.junit.Assert.assertEquals("failure - not equal", 2, i);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+
+}
---
+++ java/iteration/FooTest.java
@@ -0,0 +1,110 @@
+package iteration;
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import java.util.*;
+
+/**
+ * Tests for {@link Foo}.
+ *
+ * @author user@example.com (John Doe)
+ */
+public class FooTest {
+
+    @Test
+    public void test1() {
+        Queue<Integer> x = new LinkedList<Integer>();
+        MyFolder<Integer, Integer> mf = new MyFolder();
+        x.add(1);
+        x.add(2);
+        x.add(3);
+        MyFunc2<Integer, Integer, Integer> mf2 = new MyFunc2();
+        
+        Integer r = mf.fold(new Integer(0), x, mf2);
+	org.junit.Assert.assertEquals("failure - not equal", new Integer(6), r);
+    }
+    
+    @Test
+    public void test2() {
+        Queue<Integer> x = new LinkedList<Integer>();
+        MyFolder<Integer, Integer> mf = new MyFolder();
+        x.add(1);
+        x.add(2);
+        x.add(3);
+        MyFunc3 mf3 = new MyFunc3();
+        
+        Integer r = mf.fold(new Integer(0), x, mf3);
+	org.junit.Assert.assertEquals("failure - not equal", 6, r.intValue());
+    }
+
+    @Test
+    public void test3() {
+        Queue<Integer> x = new LinkedList<Integer>();
+        MyFolder<Integer, List> mf = new MyFolder<Integer, List>();
+        x.add(1);
+        x.add(2);
+        x.add(3);
+        
+        myfunc4 mf4 = new myfunc4();
+        List r = mf.fold(new ArrayList<Integer>(), x, mf4);
+        Iterator<Integer> it = r.iterator();
+	int i = 1;
+        while(it.hasNext())
+	    org.junit.Assert.assertEquals("failure - not equal", it.next().intValue(), i++);
+    }
+
+    @Test
+    public void checkQueueEmpty() {
+	Queue<Integer> x = new LinkedList<Integer>();
+        MyFolder<Integer, Integer> mf = new MyFolder();
+        x.add(1);
+        x.add(2);
+        x.add(3);
+        MyFunc2<Integer, Integer, Integer> mf2 = new MyFunc2();
+        
+        Integer r = mf.fold(new Integer(0), x, mf2);
+	org.junit.Assert.assertEquals("failure - not equal", true, x.isEmpty());
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+
+    //parameterized
+    class MyFunc2<T extends Integer, U extends Integer, R extends Integer > implements Function2<T, U, R>
+    {
+	public R apply(T t, U u) 
+	{
+	    int x = u.intValue() + t.intValue();
+	    Integer i = new Integer(x);
+	    return (R)i;
+        
+	}
+    }
+
+    class MyFunc3 implements Function2<Integer, Integer, Integer>
+    {
+	public Integer apply(Integer t, Integer u) 
+	{
+	    int x = u.intValue() + t.intValue();
+	    Integer i = new Integer(x);
+	    return i;
+        
+	}
+    }
+
+    class myfunc4 implements Function2<Integer, List, List>
+    {
+	public List apply(Integer t, List u)
+	{
+	    u.add(t);
+	    return u;
+	}
+    }
+
+}
