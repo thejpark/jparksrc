7d35a6995d9a46e8fe480f7202bcdf0f2e81ade6
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-11-18 20:11:55 +1100

    flatten test

---
+++ java/flatten/FooTest.java
@@ -0,0 +1,35 @@
+package flatten;
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import java.util.*;
+
+/**
+ * Tests for {@link Foo}.
+ *
+ * @author user@example.com (Jung Gyu Park)
+ */
+public class FooTest {
+
+    @Test
+    public void test1() {
+	int[] a = {1, 5, 4, 9, 6};
+        Tree<Integer> n = Tree.Node.tree(a[1], a[2], a[3]);
+        Tree<Integer> l1 = Tree.Leaf.leaf(a[0]);
+        Tree<Integer> l2 = Tree.Leaf.leaf(a[4]);
+        Tree<Integer> t = new Tree.Node(l1, n, l2);
+        MyFlattenTree<Integer> mt = new MyFlattenTree<Integer>();
+        List<Integer> l = mt.flattenInOrder(t);
+        Iterator<Integer> it = l.iterator();
+	int i = 0;
+        while(it.hasNext()) {
+            int p = it.next();
+	    org.junit.Assert.assertEquals("failure - not equal", a[i++], p);
+        }
+        
+    }
+}
