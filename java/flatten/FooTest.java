5afb97052e3a4542b714cddd75ff652f2746bf33
U-RESMED\junggyup <junggyup@WRES24057.corp.resmed.org>
2014-11-20 14:30:24 +1100

    Add another test

--- java/flatten/FooTest.java
+++ java/flatten/FooTest.java
@@ -30,6 +30,23 @@ public class FooTest {
             int p = it.next();
 	    org.junit.Assert.assertEquals("failure - not equal", a[i++], p);
         }
+
+    @Test
+    public void test2() {
+	int[] a = {1, 5, 4, 9, 6, 3, 7};
+        Tree<Integer> n = Tree.Node.tree(a[1], a[2], a[3]);
+        Tree<Integer> l1 = Tree.Leaf.leaf(a[0]);
+        Tree<Integer> l2 = Tree.Leaf.leaf(a[4]);
+        Tree<Integer> t = new Tree.Node(l1, n, l2);
+        Tree<Integer> t = new Tree.Node(it, Tree.Leaf.leaf(a[5], Tree.Leaf.leaf(a[6])));
+        MyFlattenTree<Integer> mt = new MyFlattenTree<Integer>();
+        List<Integer> l = mt.flattenInOrder(t);
+        Iterator<Integer> it = l.iterator();
+	int i = 0;
+        while(it.hasNext()) {
+            int p = it.next();
+	    org.junit.Assert.assertEquals("failure - not equal", a[i++], p);
+        }
         
     }
 }
