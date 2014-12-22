f6554fdb242ea2d823d81f00a00240e5491a28f5
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-12-22 22:17:58 +1100

    add webserver

---
+++ java/webserver/WebServer.java
@@ -0,0 +1,72 @@
+package webserver;
+
+
+class WebServer {
+    // single thread
+    public service1() throws IOException {
+	ServerSocket s = new ServerSocket(80);
+	while (true) {
+	    Socket conn = s.accept();
+	    handleRequest(conn);
+	}
+    }
+
+    // thread per connection
+    public service2() throws IOException {
+	ServerSocket s = new ServerSocket(80);
+	while (true) {
+	    final Socket conn = s.accept();
+	    Runnable task = new Runnable () {
+		public void run() {
+		    handleRequest(conn);
+		}
+	    };
+	    new Thread(task).start();   
+	}
+    }
+
+    // thread pool
+    public service3() throws IOException {
+	ServerSocket s = new ServerSocket(80);
+	final int NTHREADS = 10;
+	Executor ex = Executors.newFixedThreadPool(NTHREADS);
+	while (true) {
+	    final Socket conn = s.accept();
+	    Runnable task = new Runnable () {
+		public void run() {
+		    handleRequest(conn);
+		}
+	    };
+	    ex.execute(task);
+	}
+    }
+
+}
+
+
+class WebServer2 {
+
+    WebServer2() {
+	final int NTHREADS = 10;
+	ExecutorService ex = Executors.newFixedThreadPool(NTHREADS);
+    }
+
+    // thread pool
+    public start() throws IOException {
+	ServerSocket s = new ServerSocket(80);
+	while (!ex.isShutdown()) {
+	    final Socket conn = s.accept();
+	    Runnable task = new Runnable () {
+		public void run() {
+		    handleRequest(conn);
+		}
+	    };
+	    ex.submit(task);
+	}
+    }
+
+    public stop() {
+	ex.shutdown();
+    }
+
+}
