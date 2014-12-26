eb4509add96f46a288aa96c6c076c88abb295aa8
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-12-26 19:38:50 +1100

    add parallel solver

--- java/parallelrecursive/ParallelRecursive.java
+++ java/parallelrecursive/ParallelRecursive.java
@@ -40,6 +40,7 @@ interface Pizzle<P, M> {
 class SequentialPuzzleSolver<P, M> {
     private final Puzzle<P, M> puzzle;
     private final Set<P> seen  = new HashSet();
+    
 
     SequentialPuzzleSolver(Puzzle<P, M> p) {
 	this.puzzle = p;
@@ -85,6 +86,92 @@ class SequentialPuzzleSolver<P, M> {
 	    for (Node<P, M> n = this, n.move != null; n = n.prev)
 		solution.add(0, n.move);
 	    return solution;
+	}
+    }
+}
+
+
+class ConcurrentPuzzleSolver<P, M> {
+    private final Puzzle<P, M> puzzle;
+    private final ExecutorService exec;
+    private final ConcurrentMap<P, Boolean> seen;
+    final ValueLatch<Node<P, M>> solution = new ValueLatch();
+
+    List<M> solve() throws InterruptedException {
+	try {
+	    P pos = puzzle.initialPosition();
+	    exec.execute(newTaskp, null, null);
+	    // block until solution found
+	    Node<P, M> solnNode = solution.getValue();
+	    return (solnNode == null)? null:solnNode.asMoveList();
+	} finally {
+	    exec.shutdown();
+	}
+    }
+
+    protected Runnable newTask(P p, M m, Node<P, M> n) {
+	return new SolverTask(p, m, n);
+    }
+
+    class SolverTask extends Node<P, M> implements Runnable {
+	public void run() {
+	    if (solution.isSet() ||
+		seen.putIfAbsent(pos, true) != null)
+		return;
+	    
+	    if (puzzle.isGoal(pos)) {
+		solution.setValue(this);
+	    } else {
+		for (M move : puzzle.legalMoves(pos)) {
+		    exec.execute(newTask(puzzle.move(pos, m),
+					 m,
+					 this));
+		}
+	    }
+	}
+    }
+
+    @Immutable
+    static class Node<P, M> {
+	final P pos;
+	final M move;
+	final Node<P, M> prev;
+
+	Node(P p, M m, Node<P, M> prev) {
+	    this.pos = p;
+	    this.move = m;
+	    this.prev = prev;
+	}
+    
+	List<M> asMoveList() {
+	    List<M> solution = new LinkedList();
+	    for (Node<P, M> n = this, n.move != null; n = n.prev)
+		solution.add(0, n.move);
+	    return solution;
+	}
+    }
+}
+
+
+class ValueLatch<T> {
+    private T value = null;
+    private final CountDownLatch done = new CountDownLatch(1);
+
+    public boolean isSet() {
+	return (done.getCount() == 0);
+    }
+
+    public synchronized void SetValue(T newValue) {
+	if (!isSet()) {
+	    value = newValue;
+	    done.countDown();
+	}
+    }
+
+    public T getValue() throws InterruptedException {
+	done.await();
+	synchronized(this) {
+	    return value;
 	}
     }
 }
