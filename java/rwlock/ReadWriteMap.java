df19950a586f7627a102edc659b16791b8e5c270
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2014-12-31 17:29:11 +1100

    add example of readwritelock for java

---
+++ java/rwlock/ReadWriteMap.java
@@ -0,0 +1,31 @@
+package rwlock;
+
+
+class ReadWriteMap<K, V> {
+    private final Map<K, V> map;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock r = lock.readLock();
+    private final Lock w = lock.wroteLock();
+
+    ReadWriteMap(Map<K, V> map) {
+	this.map = map;
+    }
+    
+    public V put(K key, V value) {
+	w.lock();
+	try {
+	    return map.put(key, value);
+	} finall {
+	    w.unlock();
+	}
+    }
+    
+    public V put(K key) {
+	r.lock();
+	try {
+	    return map.get(key);
+	} finall {
+	    r.unlock();
+	}
+    }
+	
