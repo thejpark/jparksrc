a73faf633e064faf0f85d53ea4e00bd032ce650f
U-RESMED\junggyup <junggyup@WRES24057.corp.resmed.org>
2015-03-05 12:16:40 +1100

    add lock ordering

---
+++ java/lockordering.java
@@ -0,0 +1,49 @@
+private static final Object tieLock = new Object();
+
+public void transferMoney(final Account fromAcct,
+                          final Account toAcct,
+                          final DollarAmount amout)
+    throws InsufficientFundException {
+    
+    class Helper {
+        public void transfer() {
+            if (fromAcct.getBalance.compareTo(amout) < 0)
+                throw new InsufficientFundException();
+            else
+            {
+                fromAcct.debit(amout);
+                toAcct.credit(amout);
+            }
+            
+        }
+    }
+
+    int fromHash = System.identityHashCode(fromAcct);
+    int toHash = System.identityHashCode(toAcct);
+
+
+    if (fromHash < toHash) {
+        synchronized(fromHash) {
+            synchronized(toHash) {
+                new Helper().transfer();
+            }
+        }
+    }
+    else if (fromHash > toHash) {
+        synchronized(toHash) {
+            synchronized(fromHash) {
+                new Helper().transfer();
+            }
+        }
+    }
+    else {
+        synchronized(tieHash) {
+            synchronized(fromHash) {
+                synchronized(toHash) {
+                    new Helper().transfer();
+                }
+            }
+        }
+    }
+}
+     
