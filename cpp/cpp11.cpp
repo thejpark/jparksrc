1746db06f96472839fdd6a98bcaace24aa79798b
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2015-03-17 22:05:41 +1100

    add semaphore. Is it good?

--- cpp/cpp11.cpp
+++ cpp/cpp11.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <vector>
 #include <future>
+#include <thread>
+#include <mutex>
+
 
 using namespace std;
 
@@ -33,9 +36,78 @@ void t2()
     cout << myfib(6) << endl;
 }
 
+
+class mysem {
+public:
+    mysem(int size) : avail{size} {}
+    mysem() : avail{0} {}
+    ~mysem() {}
+    void acquire()
+    {
+        unique_lock<mutex> lck{mmutex}; // acquire mmutex
+        while (true) {
+            if (avail > 0)
+            {
+                --avail;
+                return;
+            }
+            else
+            {
+                mcond.wait(lck); // release lck and wait
+                // reacquire lck upon wake up
+            }
+        }
+    }
+    void release()
+    {
+        unique_lock<mutex> lck{mmutex}; // acquire mmutex
+        ++avail;
+        mcond.notify_one();
+    }
+
+private:
+    int avail;
+    condition_variable mcond;
+    mutex mmutex;
+};
+
+
+static int mycount = 0;
+static mutex m;
+static mysem sem(100);
+
+void task3a()
+{
+    while (true)
+    {
+        sem.acquire();
+        unique_lock<mutex> lck{m};
+        ++mycount;
+        cout << this_thread::get_id() << " : " << mycount << endl;
+    }
+}
+
+void task3b()
+{
+    while (true)
+    {
+        sem.release();
+    }
+}
+
+void t3()
+{
+    thread a(task3a);
+    thread b(task3a);
+    // thread c(task3b);
+    a.join();
+    b.join();
+    // c.join();
+}
+
 int main(int argc, char * argv[])
 {
-	t2(); 
+	t3(); 
 }
 
 
