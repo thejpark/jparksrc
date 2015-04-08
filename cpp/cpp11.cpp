b96ddd1e42967833d54abe65aaafaf4d0b9eb7fd
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2015-04-08 23:31:53 +1000

    packaged-task, compiled with g++ 4.9

--- cpp/cpp11.cpp
+++ cpp/cpp11.cpp
@@ -12,6 +12,7 @@
 #include <thread>
 #include <mutex>
 #include <stdexcept>
+#include <numeric>
 
 using namespace std;
 
@@ -557,6 +558,27 @@ void t17()
     unique_lock<mutex> lck2{m2, defer_lock};
 
     lock(lck1, lck2);
+}
+
+double accum(double* beg, double* end, double init)
+{
+    return accumulate(beg, end, init);
+}
+
+double comp2(vector<double>& v)
+{
+    using Task_type = double(double*, double*, double);
+
+    packaged_task<Task_type> pt0 {accum};
+    packaged_task<Task_type> pt1 {accum};
+
+    future<double> f0 {pt0.get_future()};
+    future<double> f1 {pt1.get_future()};
+
+    double* first = &v[0];
+    thread t1 {move(pt0), first, first + v.size() / 2, 0.0};
+    thread t2 {move(pt1), first + v.size() / 2, first + v.size(), 0.0};
+    return f0.get() + f1.get();
 }
 
 int main(int argc, char * argv[])
