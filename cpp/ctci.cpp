5a7ff3dbc5827add12f624e937daeba3ad9ea1d5
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2015-07-04 10:40:54 +1000

    add comments for large tree matching case

--- cpp/ctci.cpp
+++ cpp/ctci.cpp
@@ -2227,6 +2227,26 @@ void t46()
 
 // You have two very large binary trees: T1, with millions of nodes, and T2, 
 // with hun- dreds of nodes Create an algorithm to decide if T2 is a subtree of T1
+
+#if 0
+Note that the problem here specifies that T1 has millions of nodes
+this means that we should be careful of how much space we use Lets say,
+ for example, T1 has 10 million nodes thismeansthatthedataaloneisabout40mb 
+Wecouldcreateastringrepresenting the inorder and preorder traversals If T2s 
+preorder traversal is a substring of T1s preorder traversal, and T2s inorder 
+traversal is a substring of T1s inorder traversal, then T2 is a sub- string
+ of T1 We can check this using a suffix tree However, we may hit memory 
+limitations because suffix trees are extremely memory intensive If this 
+become an issue, we can use an alternative approach
+                                   
+Alternative Approach: The treeMatch procedure visits each node in the small 
+tree at most once and is called no more than once per node of the large tree 
+Worst case runtime is at most O(n * m), where n and m are the sizes of trees 
+T1 and T2, respectively If k is the number of occurrences of T2s root in T1, 
+the worst case runtime can be characterized as O(n + k * m)
+
+#endif
+    
 void t47()
 {
 
