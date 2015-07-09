8856de5b0808db70f9f83f6dd7f2823947bf08c0
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2015-07-09 21:06:46 +1000

    indentation

--- java/memoizer/Memoizer.java
+++ java/memoizer/Memoizer.java
@@ -5,40 +5,40 @@ import java.util.concurrent.*;
 class Memoizer<A, V> implements Computable<A, V> {
 
     private final ConcurrentMap<A, Future<V>> cache
-	= new ConcurrentHashMap();
+        = new ConcurrentHashMap();
     private final Computable<A, V> c;
 
     Memoizer(Computable<A, V> c) {
-	this.c = c;
+        this.c = c;
     }
 
     public V compute(final A arg) throws InterruptedException {
 
-	while (true) {
-	    Future<V> f = cache.get(arg);
-	    if (f == null) {
+        while (true) {
+            Future<V> f = cache.get(arg);
+            if (f == null) {
 		
-		Callable<V> e = new Callable() {
-			public V call() throws InterruptedException {
-			    return c.compute(arg);
-			}
-		};
-		FutureTask<V> ft = new FutureTask(e);
-		f = cache.putIfAbsent(arg, ft);
-		if (f == null) {
-		    f = ft;
-		    ft.run();
-		}
-	    }
+                Callable<V> e = new Callable() {
+                        public V call() throws InterruptedException {
+                            return c.compute(arg);
+                        }
+                    };
+                FutureTask<V> ft = new FutureTask(e);
+                f = cache.putIfAbsent(arg, ft);
+                if (f == null) {
+                    f = ft;
+                    ft.run();
+                }
+            }
 
-	    try {
-		return f.get();
-	    } catch (CancellationException ce) {
-		cache.remove(arg, f);
-	    } catch (ExecutionException ee) {
-		// throw launderThrowable(ee.getCause());
-	    }
-	}
+            try {
+                return f.get();
+            } catch (CancellationException ce) {
+                cache.remove(arg, f);
+            } catch (ExecutionException ee) {
+                // throw launderThrowable(ee.getCause());
+            }
+        }
 
     }
 }
