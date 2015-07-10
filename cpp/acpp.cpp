39c9de651cc9aa5b067c623c673db9e4e901ecdf
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2015-07-10 18:04:37 +1000

    test hash

--- cpp/acpp.cpp
+++ cpp/acpp.cpp
@@ -4002,6 +4002,23 @@ void test_template()
 
 }
 
+
+void test_hash ()
+{
+    char nts1[] = "Test";
+    char nts2[] = "Test";
+    std::string str1 (nts1);
+    std::string str2 (nts2);
+
+    std::hash<char*> ptr_hash;
+    std::hash<std::string> str_hash;
+
+    std::cout << "same hashes:\n" << std::boolalpha;
+    std::cout << "nts1 and nts2: " << (ptr_hash(nts1)==ptr_hash(nts2)) << '\n';
+    std::cout << "str1 and str2: " << (str_hash(str1)==str_hash(str2)) << '\n';
+}
+
+
 int main(int argc, char * argv[])
 {
     test_template();
