c545742d1683050467595112c6a5633de48cba7f
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2015-09-04 21:00:59 +1000

    added parse_hanja.py

---
+++ python/parse_hanja.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+
+from HTMLParser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.script_found = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'script':
+            self.script_found = True
+
+    def handle_endtag(self, tag):
+        pass
+
+    def handle_data(self, data):
+        if self.script_found:
+            self.get_hangul(data)
+            self.script_found = False
+
+    def get_hangul(self, data):
+        index = data.find('value:')
+        hangul = data[index + 7: index + 10]
+        print hangul
+
+
+def process_file(file_name):
+    
+    # instantiate the parser and fed it some HTML
+    parser = MyHTMLParser()
+
+    s = ''
+    with open(file_name, "r") as rf:
+        s = rf.read()
+
+    parser.feed(s);
+
+if __name__ == '__main__':
+    process_file('3.txt')
+    print 'Done'
+
+
