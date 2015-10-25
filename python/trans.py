55eef04b599d35cba312406f2392e58feb2e6d8e
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2015-10-25 17:38:55 +1100

    added trans.py

---
+++ python/trans.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import urllib
+import urllib2
+import threading
+import time
+
+def load():
+    r = ''
+    with open('final.txt', 'r') as rf:
+        for line in rf:
+            if line[:3] == '***':
+                r += '"' + line[3:-1] + '":'
+            else:
+                s = line.split(':')
+                if len(s) == 3:
+                    r += '("' + s[0] + '","' + s[1] + '",' + s[2][:-1] + '),\n'
+                else:
+                    print 'parsing error at line'
+                    print line
+                    return
+
+    return r
+
+if __name__ == '__main__':
+    l = load()
+
+    print l
+
