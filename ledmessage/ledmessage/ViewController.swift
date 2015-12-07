07120092f72192cb522a75c78308bf06be085fc4
Jung Gyu Park <jpark@parks-macbook-pro.local>
2015-12-07 20:40:31 +1100

    Initial commit

---
+++ ledmessage/ledmessage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ledmessage
+//
+//  Created by Park Jung Gyu on 7/12/2015.
+//  Copyright © 2015 pyfamily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
