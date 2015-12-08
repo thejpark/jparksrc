5a84e68f2e982485424025dd598e423a94b7f059
Jung Gyu Park <jpark@parks-macbook-pro.local>
2015-12-08 22:07:14 +1100

    add video capture

--- ledmessage/ledmessage/ViewController.swift
+++ ledmessage/ledmessage/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +26,62 @@ class ViewController: UIViewController {
 //    @IBOutlet weak var start: UIButton!
 //    @IBOutlet weak var stop: UIButton!
     @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var imagePicker: UIImagePickerController!
+    var newMedia: Bool?
     
     
-    @IBAction func startCamera(sender: AnyObject?) {
-        self.message.text = "Start"
-    }
+   // @IBAction func startCamera(sender: AnyObject?) {
+   //    self.message.text = "Start"
+   // }
 
     @IBAction func stopCamera(sender: AnyObject?) {
         self.message.text = "Stop"
+    }
+    
+    @IBAction func startCamera(sender: AnyObject?) {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.Camera) {
+                
+                imagePicker = UIImagePickerController()
+
+                imagePicker.delegate = self
+                imagePicker.sourceType =
+                    UIImagePickerControllerSourceType.Camera
+                imagePicker.mediaTypes = [kUTTypeVideo as String]
+                imagePicker.allowsEditing = false
+                
+                self.presentViewController(imagePicker, animated: true, 
+                    completion: nil)
+                newMedia = true
+        }
+        else
+        {
+            self.message.text = "Start"
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        let mediaType = info[UIImagePickerControllerMediaType] as! String
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        if mediaType.isEqual([kUTTypeImage as String]) {
+            let image = info[UIImagePickerControllerOriginalImage]
+                as! UIImage
+            
+            imageView.image = image
+            
+            if (newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self,
+                    "image:didFinishSavingWithError:contextInfo:", nil)
+            }
+        } else if mediaType.isEqual([kUTTypeMovie as String]) {
+                // Code to support video here
+            
+        }
+        
     }
 }
 
