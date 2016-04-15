db96bdaf5f9d52363d704f7ec816c52ac4e26ad3
Jung Gyu Park <jpark@parks-macbook-pro.local>
2016-04-15 19:57:19 +1000

    rounded button introduced

---
+++ singleview/singleview/BorderedButton.swift
@@ -0,0 +1,24 @@
+//
+//  BorderedButton.swift
+//  BabyName
+//
+//  Created by Park Jung Gyu on 15/04/2016.
+//  Copyright © 2016 pyfamily. All rights reserved.
+//
+
+
+import UIKit
+
+class BorderedButton: UIButton {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.borderWidth = 1.0
+        layer.borderColor = tintColor.CGColor
+        layer.cornerRadius = 5.0
+        clipsToBounds = true
+        contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        setTitleColor(tintColor, forState: .Normal)
+        setTitleColor(UIColor.whiteColor(), forState: .Highlighted)
+    }
+}
