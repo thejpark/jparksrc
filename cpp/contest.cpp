3852c0c450c562963a3b413d7eb8a4076d4aa401
U-RESMED\junggyup <junggyup@AP20669.corp.resmed.org>
2016-05-31 13:04:46 +1000

    get beautier

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -3055,10 +3055,108 @@ void find_match_sum_bintree()
         }
     }
 }
+
+
+/*
+
+The beauty of a number X is the number of 1s in the binary representation of X.
+
+Two players are plaing a game. There is number n written on a black board. The game is played as following:
+
+Each time a player chooses an integer number (0 <= k) so that 2^k is less than n and (n-2^k) has as much beauty as n. Next he removes n from blackboard and writes n-2^k instead. The player that can not continue the game (there is no such k that satisfies the constrains) looses the game.
+
+The First player starts the game and they play in turns alternatively. Knowing that both two players play optimally you have to specify the winner.
+
+Input:
+
+First line of the Input contains an integer T, the number of testcase. 0 <= T <= 5.
+
+Then follow T lines, each containing an integer n.
+
+n more than 0 and less than 10^9 +1.
+
+Output
+
+For each testcase print "First Player" if first player can win the game and "Second Player" otherwise.
+
+Sample Input
+
+7 1 2 8 16 42 1000 123
+
+Sample Output
+
+Second Player First Player First Player Second Player Second Player First Player Second Player
+
+Explanation
+
+In the first example n is 1 and first player can't change it so the winner is the second player.
+
+In the second example n is 2, so the first player subtracts 1 (2^0) from n and the second player looses the game.
+
+ */
+
+
+int get_ones(int x)
+{
+    int i = 0;
+    while (x > 0)
+    {
+        x = x & (x - 1);
+        i += 1;
+    }
+
+    return i;
+}
+
+
+
+int get_btr(int r)
+{
+    int i = 1;
+    int num = get_ones(r);
+    while (r > i)
+    {
+        int x = get_ones(r - i);
+        if (x >= num)
+            return (r - i);
+        i = i * 2;
+    }
+
+    return -1;
+}
+
+
+void beauty_number()
+{
+    int x;
+    cin >> x;
+
+    int user = 0;
+    while (x > 0)
+    {
+        cout << "before " << x;
+        x = get_btr(x);
+        cout << " and after " << x << endl;
+        if (x == -1)
+            break;
+
+        user += 1;
+    }
+
+    string winner;
+    if ((user % 2) == 1)
+        winner = "user1";
+    else
+        winner = "user2";
+
+    cout <<"winner is " << winner << endl;
+}
+
+
     
 int main()
 {
-    find_match_sum_bintree();
+    beauty_number();
 }
 
 
