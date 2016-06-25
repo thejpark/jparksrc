72e3ff8ed58d814914048a36f757cb7d26b381ba
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2016-06-26 07:59:41 +1000

    what is space complexity

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -18,6 +18,23 @@ http://web.stanford.edu/class/cs97si/
 using namespace std;
 
 
+/*
+
+  space complexity:
+
+  http://www.geeksforgeeks.org/g-fact-86/
+
+Space Complexity:
+The term Space Complexity is misused for Auxiliary Space at many places. Following are the correct definitions of Auxiliary Space and Space Complexity.
+
+Auxiliary Space is the extra space or temporary space used by an algorithm.
+
+Space Complexity of an algorithm is total space taken by the algorithm with respect to the input size. Space complexity includes both Auxiliary space and space used by input.
+
+For example, if we want to compare standard sorting algorithms on the basis of space, then Auxiliary Space would be a better criteria than Space Complexity. Merge Sort uses O(n) auxiliary space, Insertion sort and Heap Sort use O(1) auxiliary space. Space complexity of all these sorting algorithms is O(n) though.
+*/
+
+
 void add()
 {
   int a, b;
