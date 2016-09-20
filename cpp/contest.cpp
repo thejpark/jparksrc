996744f120013840d5daa284a080ac174814c952
U-RESMED\junggyup <junggyup@AP20669.corp.resmed.org>
2016-09-20 13:01:54 +1000

    add comments for 2 machine n job

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -3125,6 +3125,9 @@ int process_jobs2m(int idx, vector<int>& a, vector<int>& b, int ka, int kb, int 
 }
 
 
+/*
+You are given 2 machines. There are N jobs you have to perform. Job i takes Ai time to perform on machine A and Bi time to perform on machine B. Each job should be done either on machine A or B. The jobs should be performed in order. Given the arrays A and B and an integer K, find the minimum time required to complete the jobs, given that you cannot do more than K jobs on the same machine continuously. This can be done in O(N K)space and time. This can be improved to O(N K) time and O(N ) space and further to O(N logN ) time.
+*/
 void two_machine_n_jobs()
 {
 
