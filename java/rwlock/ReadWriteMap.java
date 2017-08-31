9424229262b660f5f57693b44364a769fcd6b24f
Jung Gyu Park <jpark@Parks-MacBook-Pro.local>
2017-08-31 17:18:02 +1000

    java readwritelock

--- java/rwlock/ReadWriteMap.java
+++ java/rwlock/ReadWriteMap.java
@@ -1,30 +1,29 @@
 package rwlock;
 
 
-class ReadWriteMap<K, V> {
+class ReadWriteMap<K, V> { //jj
     private final Map<K, V> map;
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private final Lock r = lock.readLock();
     private final Lock w = lock.wroteLock();
 
     ReadWriteMap(Map<K, V> map) {
-	this.map = map;
+        this.map = map;
     }
-    
+
     public V put(K key, V value) {
-	w.lock();
-	try {
-	    return map.put(key, value);
-	} finall {
-	    w.unlock();
-	}
+        w.lock();
+        try {
+            return map.put(key, value);
+        } finall {
+            w.unlock();
+        }
     }
-    
-	r.lock();
-	try {
-	    return map.get(key);
-	} finall {
-	    r.unlock();
-	}
+
+    r.lock();
+    try {
+        return map.get(key);
+    } finall {
+        r.unlock();
     }
-	
+}
