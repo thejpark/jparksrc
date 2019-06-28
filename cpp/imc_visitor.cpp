ed872f851f6e2bd0475b1dcaa02263a4d479d1f2
Jung Gyu Park <jpark@Parks-MBP.gateway>
2019-06-29 09:46:00 +1000

    initial implementation

--- cpp/imc_visitor.cpp
+++ cpp/imc_visitor.cpp
@@ -1,15 +1,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <vector>
+#include <iostream>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
 
 struct Point {
+    Point(double a, double b) : x(a), y(b) {}
     double x, y;
 };
 
 using Area = double;
+using namespace std;
 
 class Circle;
 class Rectangle;
@@ -62,14 +66,19 @@ private:
 
 class Triangle : public Shape{
 public:
-    Triangle();
+    Triangle(Point point1, Point point2, Point point3) : p1(point1), p2(point2), p3(point3) {}
     void Accept(ShapeVisitor& visitor) override {
         visitor.Visit(*this);
     }
 
     Area GetArea() {
-        return 0;
+        // refer to: http://www.mathguru.com/level2/application-of-coordinate-geometry-2007101600011139.aspx
+        double area = fabs((p1.x - p3.x) * (p2.y - p1.y) - (p1.x - p2.x) * (p3.y - p1.y) ) / 2;
+        return area;
     }
+
+private:
+    Point p1, p2, p3;
 };
 
 class AreaVisitor : public ShapeVisitor{
@@ -95,4 +104,14 @@ private:
 
 int main()
 {
+    vector<Shape*> vs;
+    vs.emplace_back(new Circle(Point(1.0, 1.0), 2));
+    vs.emplace_back(new Rectangle(Point(1.0, 1.0), Point(3.0, 3.0)));
+    AreaVisitor visitor;
+    for (auto e : vs)
+    {
+        e->Accept(visitor);
+    }
+
+    cout << "the result is " << visitor.GetTotalArea() << endl;
 }
