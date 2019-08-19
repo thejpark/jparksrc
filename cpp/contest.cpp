313e798b94e79bd6a3b45bb1108f8911ef227677
Jung Gyu Park <thejpark@gmail.com>
2019-08-19 20:11:55 +1000

    max gap between numbers

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -5636,6 +5636,7 @@ public:
 // find max gap
 // Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
 // Return 0 if the array contains less than 2 elements.
+// BIN sort (radix sort)
 int maximumGap(vector<int>& nums)
 {
 
@@ -5676,6 +5677,43 @@ int maximumGap(vector<int>& nums)
     return maxGap;
 }
 
+// maxgap with buckets and pigeonhole principle
+class Bucket {
+public:
+    bool used = false;
+    int minval = numeric_limits<int>::max();        // same as INT_MAX
+    int maxval = numeric_limits<int>::min();        // same as INT_MIN
+};
+int maximumGap2(vector<int>& nums)
+{
+    if (nums.empty() || nums.size() < 2)
+        return 0;
+
+    int mini = *min_element(nums.begin(), nums.end()),
+        maxi = *max_element(nums.begin(), nums.end());
+
+    int bucketSize = max(1, (maxi - mini) / ((int)nums.size() - 1));        // bucket size or capacity
+    int bucketNum = (maxi - mini) / bucketSize + 1;                         // number of buckets
+    vector<Bucket> buckets(bucketNum);
+
+    for (auto&& num : nums) {
+        int bucketIdx = (num - mini) / bucketSize;                          // locating correct bucket
+        buckets[bucketIdx].used = true;
+        buckets[bucketIdx].minval = min(num, buckets[bucketIdx].minval);
+        buckets[bucketIdx].maxval = max(num, buckets[bucketIdx].maxval);
+    }
+
+    int prevBucketMax = mini, maxGap = 0;
+    for (auto&& bucket : buckets) {
+        if (!bucket.used)
+            continue;
+
+        maxGap = max(maxGap, bucket.minval - prevBucketMax);
+        prevBucketMax = bucket.maxval;
+    }
+
+    return maxGap;
+}
 
 int main()
 {
