40dca94e7a73e5e8ad396c60f5d0493fdfbffd21
Jung Gyu Park <thejpark@gmail.com>
2019-09-08 13:04:55 +1000

    change UI

--- singleview/singleview/MainViewController.swift
+++ singleview/singleview/MainViewController.swift
@@ -17,26 +17,26 @@ struct RegisterInfoKeys {
     static let editCount = "editCount"
 }
 
-// I think only insane person may try 
+// I think only insane person may try
 var gEditCount = 100000
 
 class MainViewController: UIViewController {
-    
+
     var surName: String = ""
     var surNameH: String = ""
     var dob: String = ""
     var gender: String = ""
     var place: String = ""
-    
+
     @IBOutlet weak var settings: UIBarItem!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         // load register info
         let defaults = UserDefaults.standard
-        
+
         if let str = defaults.string(forKey: RegisterInfoKeys.surName) {
             self.surName = str
         }
@@ -58,8 +58,8 @@ class MainViewController: UIViewController {
             gEditCount = Int(str2)!
         }
 
-        
-        
+
+
 /*
         self.settings.title = NSString(string: "\u{2699}") as String
         if let font = UIFont(name: "Helvetica", size: 18.0) {
@@ -68,19 +68,19 @@ class MainViewController: UIViewController {
  */
 
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
+
+
     @IBAction func enterInfo(_ sender: UIButton)
     {
         let defaults = UserDefaults.standard
-        
+
         if defaults.string(forKey: RegisterInfoKeys.surName) != nil {
-            
+
             if let str1 = defaults.string(forKey: RegisterInfoKeys.surName) {
                 self.surName = str1
             }
@@ -99,9 +99,9 @@ class MainViewController: UIViewController {
                 timeDiff = timeDiffMap[self.place]!
             }
 
-            
+
             let vc = self.storyboard!.instantiateViewController(withIdentifier: "RegisterInfoView") as! RegisterConfirmViewController
-            
+
             vc.surName = self.surName
             vc.surNameH = self.surNameH
             vc.selectedDate = self.dob
@@ -111,20 +111,20 @@ class MainViewController: UIViewController {
             self.show(vc as UIViewController, sender: vc)
         }
         else {
-            
+
             let vc = self.storyboard!.instantiateViewController(withIdentifier: "PopUpView") as! PopUpViewController
             self.addChild(vc)
             self.view.addSubview(vc.view)
-            vc.showInView("등록할까요?",  animated: true)
+            vc.showInView("출생 정보를 등록할까요?",  animated: true)
         }
     }
-    
-    
+
+
     @IBAction func enterName(_ sender: UIButton) {
 
         // load register info
         let defaults = UserDefaults.standard
-            
+
         if let str = defaults.string(forKey: RegisterInfoKeys.surName) {
             self.surName = str
         }
@@ -133,7 +133,7 @@ class MainViewController: UIViewController {
             self.addChild(vc)
             self.view.addSubview(vc.view)
             vc.showInView("개인 정보를 등록하세요",  animated: true)
-            
+
             return
         }
         if let str1 = defaults.string(forKey: RegisterInfoKeys.surNameH) {
@@ -161,9 +161,9 @@ class MainViewController: UIViewController {
         vc.place = self.place
 
         self.show(vc as UIViewController, sender: vc)
-  
+
     }
-    
+
     /*
     @IBAction func clearInfo(sender: UIBarItem) {
         let defaults = NSUserDefaults.standardUserDefaults()
@@ -172,7 +172,7 @@ class MainViewController: UIViewController {
         clearElem()
     }
     */
-    
+
     @IBAction func LoadSelectedNames(_ sender: UIBarItem)
     {
         if savedElements.count == 0 {
@@ -180,7 +180,7 @@ class MainViewController: UIViewController {
                 savedElements = s
                 if savedElements.count == 0 {
                     let vc2 = self.storyboard!.instantiateViewController(withIdentifier: "PopUpEmpty") as! PopUpViewController
-                    
+
                     self.addChild(vc2)
                     self.view.addSubview(vc2.view)
                     vc2.showInView("저장된 이름이 없습니다",  animated: true)
@@ -191,7 +191,7 @@ class MainViewController: UIViewController {
 
         let vc = self.storyboard!.instantiateViewController(withIdentifier: "SavedNames") as!
             SavedViewTableController
-                
+
         vc.LoadTable()
         self.show(vc as SavedViewTableController, sender: vc)
     }
@@ -199,25 +199,25 @@ class MainViewController: UIViewController {
     /*
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showOhang" {
-            
+
 
         }
         else if segue.identifier == "showTheories" {
-            
-            
+
+
         }
 
     }
  */
-    
+
     let male: [String] = ["민준","서준","주원","하준","예준","준우","도윤","지후","준서","지호","서진"]
     let female: [String] = ["서윤","서연","민서","지우","지민","서진","지아","지우","지유","하윤","하은","서현","지원"]
-    
+
     @IBAction func getRecommendedNames(_ sender: UIButton) {
-        
+
         // load register info
         let defaults = UserDefaults.standard
-        
+
         if let str = defaults.string(forKey: RegisterInfoKeys.surName) {
             self.surName = str
         }
@@ -226,7 +226,7 @@ class MainViewController: UIViewController {
             self.addChild(vc)
             self.view.addSubview(vc.view)
             vc.showInView("개인 정보를 등록하세요",  animated: true)
-            
+
             return
         }
         if let str1 = defaults.string(forKey: RegisterInfoKeys.surNameH) {
@@ -247,7 +247,7 @@ class MainViewController: UIViewController {
         var objects = [AnyObject]()
         let vc = self.storyboard!.instantiateViewController(withIdentifier: "CandidateView") as!
         MasterViewController
-        
+
         var names:[String]
         if self.gender == "남자" {
             names = male
@@ -255,14 +255,14 @@ class MainViewController: UIViewController {
         else {
             names = female
         }
-        
+
         for givenName in names {
             vc.search(surName, surNameH: surNameH, givenName: givenName, selectedDate: self.dob)
             if vc.objects.count > 0 {
                 objects.append(vc.objects[0])
             }
         }
-        
+
         if objects.count > 0 {
             vc.objects = objects
             self.show(vc as MasterViewController, sender: vc)
@@ -270,7 +270,7 @@ class MainViewController: UIViewController {
         }
         else {
             let vc2 = self.storyboard!.instantiateViewController(withIdentifier: "PopUpEmpty") as! PopUpViewController
-            
+
             self.addChild(vc2)
             self.view.addSubview(vc2.view)
             vc2.showInView("찾는 이름이 없습니다",  animated: true)
@@ -279,4 +279,3 @@ class MainViewController: UIViewController {
     }
 
 }
-
