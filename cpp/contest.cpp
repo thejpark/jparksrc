6de1e3939a242ea52be223bda2f9b059c62def8c
Jung Gyu Park <thejpark@gmail.com>
2019-12-02 20:13:22 +1100

    comment

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -5240,7 +5240,7 @@ int interpret(vector<char>& v, unordered_map<char, int>& m)
     return s.top();
 }
 
-// leetcode 224
+// leetcode 224, 227
 void test_interpret_using_postfix()
 {
 // How to evaluate a mathematical expression by compiler design. The program will ask the user to input a value (say n). Then user will input n lines of input each of which contains an identifier and its corresponding value. Then program will ask the user again to input a value (say m). Then user will input m lines of expressions. Calculate the final value for each of the given expression using first n lines of input. If you can't evaluate any expression from given numbers of identifiers then output 'Compilation Error'. Allowed mathematical operators are +(add), -(subtract), x(multiply), /(divide).
