962400201b894bc545e1e05dabd7e067f9d49cb9
Jung Gyu Park <thejpark@gmail.com>
2020-02-12 09:09:00 +1100

    remove unbalanced parenthesis

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -4380,6 +4380,43 @@ void test_remove_unbalanced_par()
     cout << "the result is " << s << endl;
 }
 
+// leetcode 301
+// Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+// Note: The input string may contain letters other than the parentheses ( and ).
+//     Example 1:
+//       Input: "()())()"
+//       Output: ["()()()", "(())()"]
+// https://leetcode.com/problems/remove-invalid-parentheses/discuss/498070/Python-100
+/* python code
+def removeInvalidParentheses(self, s: str) -> List[str]:
+   def valid(s):
+        count = 0
+        for i in s:
+            if i == "(": count += 1;
+            elif i == ")": count -= 1;
+            if count < 0: return False;
+        return count == 0;
+   def bfs(s):
+        queue = [s]
+        visited = {}
+        res = []
+        found = False
+        while queue:
+            temp = queue.pop(0)
+            if temp in visited:
+                continue;
+            visited[temp] = True
+            if valid(temp): 
+                found = True
+                res.append(temp)
+            if found: continue
+            for i in range(len(temp)):
+                if temp[i] == "(" or temp[i] == ")":
+                    new = temp[:i] + temp[i+1:]
+                    queue.append(new)
+        return res
+   return bfs(s) 
+*/
 void test_reverse_sum() //jj
 {
     string a, b;
