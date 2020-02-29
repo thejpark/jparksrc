7ae9d983e27fd4fd5bca995d1290a13e1fb7df2c
Jung Gyu Park <thejpark@gmail.com>
2020-02-29 18:17:02 +1100

    ramdonized set

--- cpp/ctci.cpp
+++ cpp/ctci.cpp
@@ -1653,6 +1653,61 @@ public:
     }
 };
 
+// leetcode 380
+// Design a data structure that supports all following operations in average O(1) time.
+
+//     insert(val): Inserts an item val to the set if not already present.
+//     remove(val): Removes an item val from the set if present.
+//     getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+class RandomizedSet {
+private:
+    vector<int> v;
+    unordered_map<int, int> m;
+    int last {-1};
+
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if (m.count(val))
+            return false;
+
+        ++last;
+        v.push_back(val);
+        m[val] = last;
+
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if(!m.count(val))
+            return false;
+
+        int idx = m[val];
+        m.erase(val);
+        if (idx != last)
+        {
+            v[idx] = v[last];
+            m[v[idx]] = idx;
+
+        }
+
+        v.pop_back();
+        --last;
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    int getRandom() {
+        int r = rand() % (v.size());
+        return v[r];
+    }
+};
 
 // partition int array with k, so that k puts together, smaller than k goes left,//jj
 // and bigger than k goes to right.
