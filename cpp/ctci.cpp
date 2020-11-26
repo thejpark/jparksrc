352d5e53c88b16f043c963ed3092b562e700aa63
Jung Gyu Park <jpark@MacBook-Pro.modem>
2020-11-26 13:03:25 +1100

    leetcode 373

--- cpp/ctci.cpp
+++ cpp/ctci.cpp
@@ -3980,6 +3980,63 @@ bool increasingTriplet(vector<int>& nums) {
 }
 
 
+// leetcode 373
+// You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+// Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+//   Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+
+//   Example 1:
+//                                         Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+//   Output: [[1,2],[1,4],[1,6]]
+//   Explanation: The first 3 pairs are returned from the sequence:
+//                                         [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+
+  using elem = pair<int, pair<int, int>>;
+  vector<elem> v;
+
+  auto comp = [](elem&a, elem &b) {
+
+                return a.first < b.first;
+
+              };
+
+  priority_queue<elem, vector<elem>, decltype(comp)> pq(comp);
+
+
+  for (int i = 0; i < nums1.size() && i < k; ++i)
+    {
+      for (int j = 0; j < nums2.size() && j < k; ++j)
+        {
+          if (pq.size() < k)
+            {
+              pq.push(elem(nums1[i] + nums2[j], {nums1[i], nums2[j]}));
+            }
+          else
+            {
+              if (nums1[i] + nums2[j] < pq.top().first)
+                {
+                  pq.push(elem(nums1[i] + nums2[j], {nums1[i], nums2[j]}));
+                  pq.pop();
+                }
+            }
+        }
+    }
+
+  vector<vector<int>> r;
+
+  while (!pq.empty())
+    {
+      auto x = pq.top();
+      r.push_back({x.second.first, x.second.second});
+      pq.pop();
+    }
+
+  return r;
+
+}
+
+
 class  MaxSumSubMatrix{
     // Given a non-empty 2D matrix matrix and an integer k, find the max sum of a rectangle in the matrix such that its sum is no larger than k.
     //     Example:
