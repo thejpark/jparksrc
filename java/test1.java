41911468b0a9472f4a2d592f7e39aa2f6efaf61c
Jung Gyu Park <thejpark@gmail.com>
2021-04-26 16:29:44 +1000

    add rate limiter

--- java/test1.java
+++ java/test1.java
@@ -568,3 +568,77 @@ class Skyline {
 
     }
 }
+class SummaryRanges {
+    // from discussion
+    /** Initialize your data structure here. */
+    TreeSet<Integer> set;
+    int max = 0;
+    int min = Integer.MAX_VALUE;
+    public SummaryRanges() {
+        set = new TreeSet<>();
+    }
+
+    public void addNum(int val) {
+        set.add(val);
+        max = Math.max(max, val);
+        min = Math.min(min, val);
+    }
+
+    public List<int[]> getIntervals() {
+        List<int[]> list = new ArrayList<>();
+        int i = min;
+        while (i <= max) {
+            if (!set.contains(i)) {
+                i++;
+                continue;
+            }
+            int j = i + 1;
+            while (j <= max && set.contains(j)) {
+                j++;
+            }
+            list.add(new int[] {i, j - 1});
+            i = j;
+        }
+        return list;
+    }
+}
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.addNum(val);
+ * int[][] param_2 = obj.getIntervals();
+ */
+
+
+
+public class RateLimiter {
+    public int REQUEST_LIMIT = 100;
+    public Long TIME_LIMIT = 1000L;
+    public class HitCounter {
+        public Queue<Long> queue;
+        public HitCounter() {
+            queue = new LinkedList<>();
+        }
+        public boolean hit(long timestamp) {
+            /* when a timestamp hit, we should poll all the timestamp before TIME_LIMIT*/
+            while (!queue.isEmpty() && queue.peek() - timestamp >= TIME_LIMIT) queue.poll();
+            if (queue.size() < 500) {
+                queue.add(timestamp); return true;
+            }
+            return false;
+        }
+    }
+    public HashMap<String, HitCounter> clientTimeStampMap = new HashMap<>();
+    public boolean isAllow(String clientId) {
+        long currTime = System.currentTimeMillis();
+        if (!clientTimeStampMap.containsKey(clientId)) {
+            HitCounter h = new HitCounter();
+            h.hit(currTime); return true;
+        } else {
+            HitCounter h = clientTimeStampMap.get(clientId);
+            return h.hit(currTime);
+        }
+    }
+
+}
