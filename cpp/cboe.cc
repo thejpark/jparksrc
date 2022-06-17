a18c12ee30ac5ea53d22f62cb0e2c3faf61a19ab
junggyu-sc <jung.gyu@safetyculture.io>
2022-06-17 17:15:05 +1000

    cleanup

--- cpp/cboe.cc
+++ cpp/cboe.cc
@@ -58,49 +58,111 @@ enum FORMAT : int {
 }
 } // namespace MESSAGE
 
-// todo:
-// 1. getline bulk read, 2. use partition instead of pq?  3. stoi order id? 4. use trie instead of map for symbol search?
-class Solution {
-public:
-std::vector<std::pair<std::string, int>>  CollectTopK(std::istream& in, int k) {
+// PITCH message wrapper
+class Message {
+private:
+    std::istream& mIn;
+    std::string message;
     using ADD = PITCH_CBOE::MESSAGE::ADD_ORDER::FORMAT;
     using EXE = PITCH_CBOE::MESSAGE::ORDER_EXECUTED::FORMAT;
     using TRD = PITCH_CBOE::MESSAGE::TRADE::FORMAT;
 
-    std::string str;
-    str.reserve(1024);
-    while (std::getline(in, str))
-    {
-        switch(str[ADD::MESSAGE_TYPE_OFS]) {
-            case 'A':
-                // order added
-                order_to_symbol[str.substr(ADD::ORDER_ID_OFS, ADD::ORDER_ID_LEN)] = str.substr(ADD::SYMBOL_OFS, ADD::SYMBOL_LEN);
-                break;
+public:
+    Message(std::istream& in) : mIn(in) {
+        message.reserve(128);
+    }
+    bool HasNext() {
+        return std::getline(mIn, message).good();
+    }
 
-            case 'P':
-                // trade
-                symbol_trade_volume[str.substr(TRD::SYMBOL_OFS, TRD::SYMBOL_LEN)] += std::stoi(str.substr(TRD::SHARES_OFS, TRD::SHARES_LEN));
-                break;
+    void Next() {
+    }
 
-            case 'E':
-                // order executed
-            {
-                const std::string& symbol = order_to_symbol[str.substr(EXE::ORDER_ID_OFS, EXE::ORDER_ID_LEN)];
-                symbol_trade_volume[symbol] += std::stoi(str.substr(EXE::SHARES_OFS, EXE::SHARES_LEN));
-            }
-                break;
+    char Type() {
+        return message[ADD::MESSAGE_TYPE_OFS];
+    }
 
-            case 'X':
-            default:
-                break;
+    std::string OrderId() {
+        if (message[ADD::MESSAGE_TYPE_OFS] == 'A') {
+            return message.substr(ADD::ORDER_ID_OFS, ADD::ORDER_ID_LEN);
+        } else {
+            return message.substr(EXE::ORDER_ID_OFS, EXE::ORDER_ID_LEN);
         }
     }
 
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    std::string Symbol() {
+        if (message[ADD::MESSAGE_TYPE_OFS] == 'A') {
+            return message.substr(ADD::SYMBOL_OFS, ADD::SYMBOL_LEN);
+        } else {
+            return message.substr(TRD::SYMBOL_OFS, TRD::SYMBOL_LEN);
+        }
+    }
 
-    #if 0
+    int Share() {
+        if (message[ADD::MESSAGE_TYPE_OFS] == 'P') {
+            return std::stoi(message.substr(TRD::SHARES_OFS, TRD::SHARES_LEN));
+        } else {
+            return std::stoi(message.substr(EXE::SHARES_OFS, EXE::SHARES_LEN));
+        }
+    }
+};
+
+class Solution {
+public:
+
+std::vector<std::pair<std::string, int>>  CollectTopK(std::istream& in, int k) {
+    Message message(in);
+
+    // compute the volume of each symbol and store in a map symbol_trade_volume.
+    while (message.HasNext())
+    {
+      message.Next();
+
+      switch (message.Type()) {
+      case 'A':
+        // order added
+        order_to_symbol[message.OrderId()] = message.Symbol();
+        break;
+
+      case 'P':
+        // trade
+        symbol_trade_volume[message.Symbol()] += message.Share();
+        break;
+
+      case 'E':
+        // order executed
+        {
+          const std::string& symbol = order_to_symbol[message.OrderId()];
+          symbol_trade_volume[symbol] += message.Share();
+        }
+        break;
+
+      case 'X':
+      default:
+        break;
+      }
+    }
+
+     #if 1
+    // Partition the symbols so that first k elements are the top k. O(n).
+    using elem = std::pair<std::string, int>;
+    auto comp = [](const elem &a, const elem& b) {
+        return a.second > b.second;
+    };
+    std::vector<elem> result{symbol_trade_volume.begin(), symbol_trade_volume.end()};
+    if (result.size() > k) {
+      std::nth_element(result.begin(), result.begin() + k - 1, result.end(), comp);
+      result.resize(k);
+    }
+
+    // Then sort based on the volume size.
+    sort(result.begin(), result.end(), comp);
+    return result;
+    // Tested with the minimum heap implementation below, and this one is about 1.5 times faster.
+
+     #else
     // use min heap to store top k. The size of heap is k where k is 10.
-    // Time complexity is O(n logk) where k is 10, so it is almsot O(n).
+    // Time complexity is O(n logk) where k is 10, so it is also O(n).
     using elem = std::pair<std::string, int>;
     auto comp = [](const elem &a, const elem& b) {
         return a.second > b.second;
@@ -127,26 +189,8 @@ std::vector<std::pair<std::string, int>>  CollectTopK(std::istream& in, int k) {
         min_heap.pop();
     }
     reverse(result.begin(), result.end());
-    #else
-
-    // partition the symbols so that first k elements are the top k.
-    using elem = std::pair<std::string, int>;
-    auto comp = [](const elem &a, const elem& b) {
-        return a.second > b.second;
-    };
-    std::vector<elem> result{symbol_trade_volume.begin(), symbol_trade_volume.end()};
-    if (result.size() > k) {
-      std::nth_element(result.begin(), result.begin() + k - 1, result.end(), comp);
-      result.resize(k);
-    }
-    // Then sort based on the volume size.
-    sort(result.begin(), result.end(), comp);
-    #endif
-
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
     return result;
-
+    #endif
 }
 
 private:
@@ -247,145 +291,6 @@ int GetIndexToSymbol(const std::string_view& sv) {
     }
 }
 
-};
-
-class Message {
-private:
-    std::istream& mIn;
-    std::string message;
-    using ADD = PITCH_CBOE::MESSAGE::ADD_ORDER::FORMAT;
-    using EXE = PITCH_CBOE::MESSAGE::ORDER_EXECUTED::FORMAT;
-    using TRD = PITCH_CBOE::MESSAGE::TRADE::FORMAT;
-
-public:
-    Message(std::istream& in) : mIn(in) {
-        message.reserve(128);
-    }
-    bool HasNext() {
-        return std::getline(mIn, message).good();
-    }
-
-    void Next() {
-    }
-
-    char Type() {
-        return message[ADD::MESSAGE_TYPE_OFS];
-    }
-
-    std::string OrderId() {
-        if (message[ADD::MESSAGE_TYPE_OFS] == 'A') {
-            return message.substr(ADD::ORDER_ID_OFS, ADD::ORDER_ID_LEN);
-        } else {
-            return message.substr(EXE::ORDER_ID_OFS, EXE::ORDER_ID_LEN);
-        }
-    }
-
-    std::string Symbol() {
-        if (message[ADD::MESSAGE_TYPE_OFS] == 'A') {
-            return message.substr(ADD::SYMBOL_OFS, ADD::SYMBOL_LEN);
-        } else {
-            return message.substr(TRD::SYMBOL_OFS, TRD::SYMBOL_LEN);
-        }
-    }
-
-    int Share() {
-        if (message[ADD::MESSAGE_TYPE_OFS] == 'P') {
-            return std::stoi(message.substr(TRD::SHARES_OFS, TRD::SHARES_LEN));
-        } else {
-            return std::stoi(message.substr(EXE::SHARES_OFS, EXE::SHARES_LEN));
-        }
-    }
-};
-
-class Solution3 {
-public:
-
-std::vector<std::pair<std::string, int>>  CollectTopK(std::istream& in, int k) {
-    Message message(in);
-
-    while (message.HasNext())
-    {
-      message.Next();
-
-      switch (message.Type()) {
-      case 'A':
-        // order added
-        order_to_symbol[message.OrderId()] = message.Symbol();
-        break;
-
-      case 'P':
-        // trade
-        symbol_trade_volume[message.Symbol()] += message.Share();
-        break;
-
-      case 'E':
-        // order executed
-        {
-          const std::string& symbol = order_to_symbol[message.OrderId()];
-          symbol_trade_volume[symbol] += message.Share();
-        }
-        break;
-
-      case 'X':
-      default:
-        break;
-      }
-    }
-
-     #if 1
-    // Partition the symbols so that first k elements are the top k.
-    using elem = std::pair<std::string, int>;
-    auto comp = [](const elem &a, const elem& b) {
-        return a.second > b.second;
-    };
-    std::vector<elem> result{symbol_trade_volume.begin(), symbol_trade_volume.end()};
-    if (result.size() > k) {
-      std::nth_element(result.begin(), result.begin() + k - 1, result.end(), comp);
-      result.resize(k);
-    }
-    // Then sort based on the volume size.
-    sort(result.begin(), result.end(), comp);
-    return result;
-    // Tested with the minimum heap implementation below, and this one is about 1.5 times faster.
-
-     #else
-    // use min heap to store top k. The size of heap is k where k is 10.
-    // Time complexity is O(n logk) where k is 10, so it is almsot O(n).
-    using elem = std::pair<std::string, int>;
-    auto comp = [](const elem &a, const elem& b) {
-        return a.second > b.second;
-    };
-
-    std::priority_queue<elem, std::vector<elem>, decltype(comp)> min_heap(comp);
-    for (auto& [symbol, size] : symbol_trade_volume) {
-        if (min_heap.size() < k) {
-            min_heap.push({symbol, size});
-        } else {
-            // if the size of heap is 10, then the top element is the smallest one.
-            // For optimisation, only consider adding to heap if the new element is larger than the top element.
-            if (min_heap.top().second < size) {
-                min_heap.pop();
-                min_heap.push({symbol, size});
-            }
-        }
-    }
-
-    // take top k out of min heap
-    std::vector<elem> result;
-    while (!min_heap.empty()) {
-        result.push_back(min_heap.top());
-        min_heap.pop();
-    }
-    reverse(result.begin(), result.end());
-    return result;
-    #endif
-}
-
-private:
-    // a map from order id to symbol name
-    std::unordered_map<std::string, std::string> order_to_symbol;
-    // for each symbol, the trade volume
-    std::unordered_map<std::string, int> symbol_trade_volume;
 };
 
 
