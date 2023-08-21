c96d00cc6664b25db1ee2ff75a2fc99c57708455
Jung Gyu Park <jung.gyu@safetyculture.io>
2023-08-22 09:07:32 +1000

    add more

--- cpp/ctci.cpp
+++ cpp/ctci.cpp
@@ -5948,6 +5948,52 @@ class ghost {
 
 };
 
+// leetcode 446. Arithmetic Slices II - Subsequence
+// Given an integer array nums, return the number of all the arithmetic subsequences of nums.
+// A sequence of numbers is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+// For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are arithmetic sequences.
+// For example, [1, 1, 2, 5, 7] is not an arithmetic sequence.
+// A subsequence of an array is a sequence that can be formed by removing some elements (possibly none) of the array.
+// For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
+// The test cases are generated so that the answer fits in 32-bit integer.
+
+// Example 1:
+
+// Input: nums = [2,4,6,8,10]
+// Output: 7
+// Explanation: All arithmetic subsequence slices are:
+// [2,4,6]
+// [4,6,8]
+// [6,8,10]
+// [2,4,6,8]
+// [4,6,8,10]
+// [2,4,6,8,10]
+// [2,6,10]
+
+   int numberOfArithmeticSlices(vector<int>& A) {
+        if (A.size() < 3)
+            return 0;
+
+        vector<unordered_map<long, int>> m(A.size());
+        int cnt = 0;
+        for (int i = 0; i < A.size(); ++i)
+        {
+            for (int j = i + 1; j < A.size(); ++j)
+            {
+                long diff = A[j];
+                diff -= A[i];
+
+                if (m[i].count(diff)) {
+                    cnt += m[i][diff];
+                    m[j][diff] += m[i][diff];
+                }
+
+                m[j][diff]++;
+            }
+        }
+        return cnt;
+    }
+
 
 // leetcode 978, longest turbelent subarray
 // 9, 4, 2, 10, 7, 8, 8, 1, 9 -> 5.  because 4, 2, 10, 7, 8 are turbelent.
